ffd9286a2c6c119ddc49337b47f7a6db754f9a1d
Mobin Dariush <dalisyron@gmail.com>
2020-03-03 17:32:23 +0330

    Add FilmDataSource

---
+++ src/datasource/FilmDataSource.java
@@ -0,0 +1,12 @@
+package datasource;
+
+import file.FileHandler;
+
+public class FilmDataSource {
+
+    private FileHandler fileHandler;
+
+    public FilmDataSource(FileHandler fileHandler) {
+        this.fileHandler = fileHandler;
+    }
+}
