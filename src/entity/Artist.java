d0e01ba20cdb1c11aae28791da0a9947bf7baf42
Mobin Dariush <dalisyron@gmail.com>
2020-03-03 13:52:57 +0330

    Add entities

---
+++ src/entity/Artist.java
@@ -0,0 +1,34 @@
+package entity;
+
+import java.util.List;
+
+public class Artist {
+
+    private final int id;
+    private final String name;
+    private final int age;
+    private final List<Film> films;
+
+    public Artist(int id, String name, int age, List<Film> films) {
+        this.id = id;
+        this.name = name;
+        this.age = age;
+        this.films = films;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public List<Film> getFilms() {
+        return films;
+    }
+}
---
+++ src/entity/Film.java
@@ -0,0 +1,39 @@
+package entity;
+
+public class Film {
+
+    private final int id;
+    private final String name;
+    private final String directorName;
+    private final int productionYear;
+    private final String genre;
+
+    public Film(int id, String name, String directorName, int productionYear, String genre) {
+
+        this.id = id;
+        this.name = name;
+        this.directorName = directorName;
+        this.productionYear = productionYear;
+        this.genre = genre;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getDirectorName() {
+        return directorName;
+    }
+
+    public int getProductionYear() {
+        return productionYear;
+    }
+
+    public String getGenre() {
+        return genre;
+    }
+}
